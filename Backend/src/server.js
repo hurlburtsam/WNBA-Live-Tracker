b025ce1ffe9a51f2804eb6bcd4e296058bb59973
Sam Hurlburt <hurlburtsam@my.easternct.edu>
2026-08-12 19:21:37 -0400

    Configured frontend and backend + test server

---
+++ Backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors({origin: process.env.CLIENT_ORIGIN}));
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.json({status: 'ok'});
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT,() => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
